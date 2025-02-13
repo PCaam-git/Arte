728e2e95c3f64a0436e9ee6ca5b51d2e51fc2e6b
P.Caam <pcaamano@outlook.es>
2025-02-13 10:37:27 +0100

    Agrego .gitignore, soluciono cambios en finales de linea y modifico errores para el funcionamiento del CRUD

--- Backend/src/controller/arte.js
+++ Backend/src/controller/arte.js
@@ -13,11 +13,24 @@ const getObra = async (req, res) => {
     res.json(obra);
 };
 
-const postObra = async (req, res) => {
+/*const postObra = async (req, res) => {
     const { descripcion, fecha_creacion, precio, imagen, ID_disciplina } = req.body;
     await registerObra({ descripcion, fecha_creacion, precio, imagen, ID_disciplina });
     res.status(201).json({ message: 'Obra creada' });
+};*/
+
+const postObra = async (req, res) => {
+    console.log("Datos recibidos:", req.body);  // <-- Agregar esta línea
+    const { ID_disciplina, ID_subdisciplina, descripcion, fecha_creacion, imagen, precio } = req.body;
+
+    if (!descripcion || !fecha_creacion || !imagen || !precio || !ID_disciplina || !ID_subdisciplina) {
+        return res.status(400).json({ error: "Faltan campos obligatorios" });
+    }
+
+    await registerObra({ ID_disciplina, ID_subdisciplina, descripcion, fecha_creacion, imagen, precio });
+    res.status(201).json({ message: "Obra creada" });
 };
+
 
 const putObra = async (req, res) => {
     const { descripcion, fecha_creacion, precio, imagen } = req.body;
